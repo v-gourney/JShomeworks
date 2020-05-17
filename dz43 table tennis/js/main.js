a62df27123aecbcc946e87a0fbbd00d9051046fc
v-gourney <61472298+v-gourney@users.noreply.github.com>
2020-05-17 18:14:54 +0300

    dz43 table tennis
    v.1

---
+++ dz43 table tennis/js/main.js
@@ -0,0 +1,204 @@
+"use strict";
+//speed and position randomizer
+function randomDiap(n,m) {
+    return Math.floor(Math.random()*(m-n+1))+n;
+}
+const ball = document.getElementById ('ball')
+const redScore = document.getElementById ('redScore');
+const blueScore = document.getElementById ('blueScore');
+const win = document.getElementById ('win');
+const startStop = document.getElementById ('startstop');
+let timer = 0;
+let audiowin = new Audio('./media/cheer.mp3');
+let audiobounce = new Audio('./media/bounce.mp3');
+let tickbounce = new Audio('./media/tick.mp3');
+let audiogong = new Audio('./media/gong.mp3');
+
+////////////////////
+const ballH = {
+    posX : randomDiap(250,450),
+    posY : randomDiap(40,300),
+    speedX : randomDiap(2,5),
+    speedY : randomDiap(2,4),
+    width : 20,
+    height: 20,
+    update : function() {
+        ball.style.left=this.posX+"px";
+        ball.style.top=this.posY+"px";
+    }
+}
+const areaH = {
+    width : 730,
+    height : 400
+}
+// paddles
+const bluePad = {
+    posY : 150,
+    speedY : 15,
+    width : 10,
+    height: 100,
+    move: function () {
+        const leftPaddle = document.getElementById('paddleBlue')
+        leftPaddle.style.top=this.posY+"px"
+    }
+}
+const redPad = {
+    posY : 150,
+    speedY : 15,
+    width : 10,
+    height: 100,
+    move: function () {
+        const rightPaddle = document.getElementById('paddleRed')
+        rightPaddle.style.top=this.posY+"px"
+    }
+}
+///////////////////////////////
+// paddles movements
+document.addEventListener('keydown', (event)=> {
+    switch(event.code) {
+        case 'ShiftLeft':  
+            if (bluePad.posY <= 0) {
+                bluePad.posY = 0;
+                break
+            }
+            bluePad.posY -= bluePad.speedY;
+            bluePad.move();
+            break
+        case 'ControlLeft':  
+            if (bluePad.posY + bluePad.height >= 400) {
+                bluePad.posY = 300;
+                break
+            }
+            bluePad.posY += bluePad.speedY;
+            bluePad.move();
+            break
+        case 'ArrowUp':  
+            if (redPad.posY <= 0) {
+                redPad.posY = 0;
+                break
+            }
+            redPad.posY -= redPad.speedY;
+            redPad.move();
+            break
+        case 'ArrowDown':  
+            if (redPad.posY + redPad.height >= 400) {
+                redPad.posY = 300;
+                break
+            }
+            redPad.posY += redPad.speedY;
+            redPad.move();
+            break
+    }
+});
+
+
+
+//start stop button function
+startStop.addEventListener ('click', start);
+function start() {
+    blueScore.value = 0;
+    redScore.value = 0;
+    win.innerHTML = '';
+    ball.style.display = 'block'
+    timer = setInterval(tick, 10);
+    audiogong.play();
+    startStop.value = 'Stop';
+    startStop.removeEventListener('click', start);
+    startStop.addEventListener('click', stop);
+}
+function stop () {
+    clearInterval (timer);
+    startStop.value = 'Start playing';
+    startStop.removeEventListener('click', stop);
+    startStop.addEventListener('click', start);
+}
+
+//movement function
+function tick() {
+    ballH.posX+=ballH.speedX;
+    ballH.posY+=ballH.speedY;
+    ballH.update();
+    
+    //red paddle hit or not
+    if ((ballH.posX+ballH.width >= areaH.width) && ((ballH.posY + ballH.height / 2) < (redPad.posY)||(ballH.posY + ballH.height / 2) > (redPad.posY + redPad.height))) {
+        ++blueScore.value;
+        tickbounce.play();
+        ballH.speedX=-ballH.speedX;
+        ballH.posX=areaH.width-ballH.width;
+    } 
+    if ((ballH.posX+ballH.width >= areaH.width - redPad.width) && ((ballH.posY + ballH.height / 2) >= (redPad.posY)&&(ballH.posY + ballH.height / 2) <= (redPad.posY + redPad.height))) {
+        audiobounce.play();
+        ballH.speedX = - (ballH.speedX + 0.5) ;
+        ballH.posX=areaH.width-ballH.width-redPad.width;
+    }
+        
+    //blue paddle hit or not
+    if ((ballH.posX <= 0) && ((ballH.posY + ballH.height / 2) < (bluePad.posY)||(ballH.posY + ballH.height / 2) > (bluePad.posY + bluePad.height))) {
+        ++redScore.value;
+        tickbounce.play();
+        ballH.speedX=-ballH.speedX;
+        ballH.posX = 0;
+    } 
+    if ((ballH.posX <= bluePad.width) && ((ballH.posY + ballH.height / 2) >= (bluePad.posY)&&(ballH.posY + ballH.height / 2) <= (bluePad.posY + bluePad.height))) {
+        audiobounce.play();
+        ballH.speedX = - (ballH.speedX - 0.5) ;
+        ballH.posX = redPad.width;
+    }
+    // вылетел ли мяч ниже пола?
+    if ( ballH.posY+ballH.height>areaH.height ) {
+        ballH.speedY=-ballH.speedY;
+        ballH.posY=areaH.height-ballH.height;
+    }
+    // вылетел ли мяч выше потолка?
+    if ( ballH.posY<0 ) {
+        ballH.speedY=-ballH.speedY;
+        ballH.posY=0;
+    }
+
+    // win conditions
+    if (blueScore.value == 10) {
+        win.innerHTML = "BLUE PLAYER WINS!"
+        win.style.color = "blue"
+        ball.style.display = 'none'
+        audiowin.play();
+
+        stop ();
+        ballH.posX = randomDiap(250,450),
+        ballH.posY = randomDiap(40,300),
+        ballH.speedX = randomDiap(2,5);
+        ballH.speedY = randomDiap(2,4);
+    } else if (redScore.value == 10) {
+        win.innerHTML = "RED PLAYER WINS!"
+        win.style.color = "red"
+        ball.style.display = 'none'
+        audiowin.play();
+
+        stop (); 
+        ballH.posX = randomDiap(250,450),
+        ballH.posY = randomDiap(40,300),
+        ballH.speedX = randomDiap(2,5);
+        ballH.speedY = randomDiap(2,4);
+    }
+}
+ballH.update();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
